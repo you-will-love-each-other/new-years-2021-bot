82e76fc39cd7f7f1445cef6676b97a04cf5e8e4d
joão <74837451+slocknad@users.noreply.github.com>
2021-12-20 17:43:49 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,9 @@ import variables
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 bot.remove_command('help')
-oldperms = dict()
-roles = [variables.killereliteID, variables.patron1, variables.patron2, variables.patron3,variables.eliteID,variables.nightmareID,variables.hurtID,variables.imtooID,variables.torquefestID,variables.adventurerID,variables.ndaID]
+
+oldperm = dict()
+roles = [variables.eliteID,variables.nightmareID,variables.hurtID,variables.imtooID,variables.torquefestID,variables.killereliteID, variables.patron1, variables.patron2, variables.patron3,variables.adventurerID,variables.ndaID,variables.nda2ID]
 
 @bot.event
 async def on_ready():
@@ -17,20 +18,59 @@ async def ping(ctx):
     await ctx.message.reply("Pong!")
 
 @bot.command()
+async def oldperms(ctx):
+    if ctx.author.id != variables.joao_id:
+        return
+    for channel in ctx.guild.channels:
+        if channel.category and channel.category.name != "MODERATOR CHAT":
+            daux = dict()
+            daux["everyone"] = channel.overwrites_for(ctx.guild.default_role)
+            for role in roles:
+                daux[str(role)] = channel.overwrites_for(ctx.guild.get_role(role))
+            oldperm[str(channel.id)] = daux
+    await ctx.reply("old perms saved.")
+
+@bot.command()
 async def start(ctx):
-    for role_id in roles:
-        role = ctx.guild.get_role(role_id)
-        perms = role.permissions
-        oldperms[str(role_id)] = perms
-        perms.view_channel = False
-        await role.edit(permissions= perms)
+    if ctx.author.id != variables.joao_id:
+        return
+    #   @everyone
+    for channel in ctx.guild.channels:
+        if channel.category and channel.category.name != "MODERATOR CHAT":
+            overwrite = oldperm[str(channel.id)]["everyone"]
+            if overwrite.read_messages:
+                overwrite.read_messages = False
+                await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+    
+    #   the rest of the roles
+    for channel in ctx.guild.channels:
+        for role_id in roles:
+            if channel.category and channel.category.name != "MODERATOR CHAT":
+                overwrite = oldperm[str(channel.id)][str(role_id)]
+                if overwrite.read_messages:
+                    overwrite.read_messages = False
+                    await channel.set_permissions(ctx.guild.get_role(role_id), overwrite=overwrite)
+    await ctx.reply("channels hidden")
 
 @bot.command()
 async def stop(ctx):
-    for role_id in roles:
-        role = ctx.guild.get_role(role_id)
-        perms = role.permissions
-        perms.view_channel = oldperms[str(role.id)]
-        await role.edit(permissions= perms)
+    if ctx.author.id != variables.joao_id:
+        return
+    #   the rest of the roles
+    for channel in ctx.guild.channels:
+        for role_id in roles:
+            if channel.category and channel.category.name != "MODERATOR CHAT":
+                overwrite = oldperm[str(channel.id)][str(role_id)]
+                if overwrite.read_messages != False:
+                    await channel.set_permissions(ctx.guild.get_role(role_id), overwrite=overwrite)
+
+    #   @everyone
+    for channel in ctx.guild.channels:
+        if channel.category and channel.category.name != "MODERATOR CHAT":
+            overwrite = oldperm[str(channel.id)]["everyone"]
+            if overwrite.read_messages != False:
+                await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+    await ctx.reply("channels are visible")
+    
 
 bot.run(variables.bottoken)
