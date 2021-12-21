134e0ae265fdac2d6989100d27f24c13e3ec86c7
joão <74837451+slocknad@users.noreply.github.com>
2021-12-21 18:47:09 +0000

    bringing back musik make love to

--- musik.py
+++ musik.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import variables
+import random
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -72,5 +73,32 @@ async def stop(ctx):
             if overwrite and overwrite.read_messages != False:
                 await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
     await ctx.reply("channels are visible")
+
+@bot.command()
+async def makeloveto(ctx, *, arg):
+    everyone = "@everyone"
+    sex1 = ["Musik Bot called with {everyone} with its index finger to bed and bit its lip", "Musik Bot touched {everyone}'s crotch, looked at {everyone} in the eyes and kissed {everyone}'s ear {everyone} was standing in the hallway, {everyone} looked at the distance and {everyone} saw Musik Bot wearing nothing but a see through black lingerie"]
+    sex2 = ["Musik Bot held {everyone}'s hand and took {everyone} to bed violently", "Musik Bot started making out with {everyone}, {everyone} took each other's clothes off and laid in bed", "Musik Bot started teasing {everyone}, touching all of its body and persuading {everyone} to go to bed with it"]
+    sex3 = ["Thank you so much for tonight, daddy {everyone}. It felt great.", "When will we do this again, {everyone}? hehe", "You are so big, I couldn't handle it, {everyone} Hehe you shouldn't had came inside me, {everyone}"]
+    option1 = random.choice(sex1)
+    option2 = random.choice(sex2)
+    option3 = random.choice(sex3)
+    await ctx.send("""
+    *{option1}*
+    *{option2}*
+    .
+    ..
+    ...
+    {everyone} and Musik Bot had sex.
+    Musik Bot turns to {everyone} and says: '*{option3}*'
+    """)
+
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith("musik make love to "):
+        message_list = message.content.split()
+        message.content = "!makeloveto {message_list[4]}"
+        await bot.process_commands(message)
 
 bot.run(variables.bot3token)
